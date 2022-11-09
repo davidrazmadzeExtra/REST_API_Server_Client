8f9447c35678ced0d8f16ca61d46a7ddbabd3a2d
David Razmadze <razd45@gmail.com>
2022-11-09 16:50:41 -0500

    finish

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-const app = require("express")();
-const PORT = 8080;
+const express = require("express");
+const app = express();
+const PORT = 8085;
+
+app.use(express.json());
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+// GET
+app.get("/tshirt", (request, res) => {
+  // 1. Send a response
+  // res.send("Hello World!");
+
+  // 2. Send a response with status code of 200 and a JSON Object
+  res.status(200).send({
+    tshirt: "🏠",
+    size: "large",
+  });
+});
