9948bb9374e89e39cf361a2fa0febc882caf1eba
David Razmadze <razd45@gmail.com>
2022-11-09 20:41:00 -0500

    Update iOS Example

--- iOS_Get_Example/iOS_Get_Example/ViewController.swift
+++ iOS_Get_Example/iOS_Get_Example/ViewController.swift
@@ -9,46 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
   
+  // MARK: - Variables
+  
+  @IBOutlet weak var iceCreamLabel: UILabel!
+  
   // MARK: - Lifecycle
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    iceCreamLabel.text = ""
     getIceCream()
   }
-
+  
   // MARK: - Helper Functions
   
   private func getIceCream() {
     // Create URL
     let url = URL(string: "http://localhost:8080/api/icecream")
     guard let requestUrl = url else { fatalError() }
-
+    
     // Create URL Request
     var request = URLRequest(url: requestUrl)
-
+    
     // Specify HTTP Method to use
     request.httpMethod = "GET"
-
+    
     // Send HTTP Request
     let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-        
-        // Check if Error took place
-        if let error = error {
-            print("❌ Error took place \(error)")
-            return
+      
+      // Check if Error took place
+      if let error = error {
+        print("❌ Error took place \(error)")
+        return
+      }
+      
+      // Read HTTP Response Status code
+      if let response = response as? HTTPURLResponse {
+        print("🔐 Response HTTP Status code: \(response.statusCode)")
+      }
+      
+      // Convert HTTP Response Data to a simple String
+      if let data = data, let dataString = String(data: data, encoding: .utf8) {
+        print("✅ Response data string:\n \(dataString)")
+        DispatchQueue.main.async {
+          self.iceCreamLabel.text = dataString
         }
-        
-        // Read HTTP Response Status code
-        if let response = response as? HTTPURLResponse {
-            print("🔐 Response HTTP Status code: \(response.statusCode)")
-        }
-        
-        // Convert HTTP Response Data to a simple String
-        if let data = data, let dataString = String(data: data, encoding: .utf8) {
-            print("✅ Response data string:\n \(dataString)")
-        }
-        
+      }
+      
     }
     task.resume()
   }
